13c7b61c78b64167d0bda21b62f91ded8dcd7078
Duong Tai <taiduong1301@gmail.com>
2025-02-02 08:16:30 +0000

    Add test

---
+++ test/README.md
@@ -0,0 +1,6 @@
+# Test Request
+
+Nếu bạn muốn sử dụng api ở nơi khác
+```bash
+docker compose up --build
+```
---
+++ test/test.py
@@ -0,0 +1,25 @@
+# script.py
+import requests
+
+# URL của ScrapyRT
+scrapyrt_url = "http://crawl:9080/crawl.json"
+
+# Dữ liệu để gửi request (nếu cần)
+payload = {
+    'start_requests': 'true',
+    'spider_name': 'google',
+    "crawl_args": {'query': 'site:www.24h.com.vn "mono"'}
+}
+
+# Gửi request đến ScrapyRT
+response = requests.post(scrapyrt_url, json=payload)
+
+# In kết quả
+if response.status_code == 200:
+    result = response.json()
+    items = result.get('items', [])
+    links = [item.get('link') for item in items]
+    print(links)
+else:
+    print(f"Error: {response.status_code}")
+    print(response.text)
